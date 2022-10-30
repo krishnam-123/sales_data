3d5d608e511bf89cb573bfb4ce0521daf4ff0d99
Krishna Joshi <joshibalkrishna1997gmail.com>
2022-10-30 11:11:53 +0530

    ADDED ALL FILES

---
+++ config.py
@@ -0,0 +1,10 @@
+
+import os
+
+CSV_FILE_PATH = "ENTERR PATH"
+
+PORT_NUMBER = 5050
+
+PICKLE_FILE_PATH = os.path.join("Sales_data","sales_product_model.pkl")
+
+JSON_FILE_PATH = os.path.join("Sales_data","sales_data_col.json")
